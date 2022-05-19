48b3913f1caef01ecb75fc611449fe1546e64b50
ilya <ilya@intertechsolution.com>
2022-05-19 22:07:48 +0300

    delete old files

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::resource('/points', \App\Http\Controllers\PointController::class);
 
 Route::resource('/category', \App\Http\Controllers\CategoryController::class);
 Route::resource('/city', \App\Http\Controllers\CityController::class);
 Route::resource('/poi',\App\Http\Controllers\PoiController::class);
 Route::resource('/type',  \App\Http\Controllers\TypeController::class);
-
-Route::post('/attach-attraction',[\App\Http\Controllers\PointController::class, 'attach']);
-Route::post('/detach-attraction',[\App\Http\Controllers\PointController::class, 'detach']);
-
-Route::resource('/attractions', \App\Http\Controllers\AttractionTypeController::class);
 
 Route::get('/', function () {
     return view('welcome');
